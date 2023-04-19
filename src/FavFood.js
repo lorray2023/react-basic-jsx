e44dce1dd55403a3d0db5f855b8b23849bd0ec62
lawrencia <lawbannwill@gmail.com>
2023-04-19 13:36:29 +0000

    done

---
+++ src/FavFood.js
@@ -0,0 +1,41 @@
+import React from "react";
+import './FavFoodStyles.css';
+
+
+function FavFood() {
+    return (
+        <div>
+            <h1>My Favorite Food</h1>
+            <p>My Favorite food is Akple and Okro soup with protein such as wele, crab, tuna and goat meat</p>
+            <p>I love to enjoy this meal with a cold filtered 1948 beer on a saturday afternoon</p>
+            <p>Hover over image to see effect of image</p>
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSzUU7z_t1WEvp_1G3dDwpQB9kzi5zte11-FQ&usqp=CAU"
+            alt="Akple and Okro soup"/>
+            <p>My second best food is Rice with Palava sauce</p>
+            <p> I love it because of it's healthy nature and will not give anything up for it</p>
+            <p>I usually like the Palava sauce freshly made with a lot of agushi and dry fish interlace with momni</p>
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR5dHvhFwgnTiRcvbKOb3Hs0-7VqJ9w6qbxkQ&usqp=CAU"
+            alt="Rice with Palaver sauce"/>
+
+
+        </div>
+    );
+
+}
+// function FavFoodStyles () {
+   
+//     return (
+//         <div>
+//             <h1 className="title"></h1>
+//             <p className="text"></p>
+//             <img className="images"/>
+
+//         </div>
+//     );
+// }
+    
+
+
+
+    
+export default FavFood;
